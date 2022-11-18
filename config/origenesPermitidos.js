6eb5d4c98a21a6e0b1d1ddcc665bae80784ba2c5
unknown <alfonsoauzmendia@gmail.com>
2022-11-18 18:26:48 +0100

    Aplicación Trabajo Deploy 1.1

--- config/origenesPermitidos.js
+++ config/origenesPermitidos.js
@@ -1,6 +1,7 @@
 const origenesPermitidos = [
   "https://www.tusitioweb.com",
   "https://aplicacion-trabajos.herokuapp.com",
+  "https://aplicacion-trabajo.onrender.com",
   "http://127.0.0.1:3000",
   "http://localhost:3000",
 ];
